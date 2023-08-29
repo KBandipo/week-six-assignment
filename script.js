4d9b92fca80b942a86aa6428a7a1e8920463f810
kennybandy <nekbandy.com>
2023-08-29 15:25:32 +0100

    Declare a variable to store sets of a,b,d amd y

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+/* create a function that will count through the string and show the number of a, b, d and y each contained in the string "abdsgyhidsaadgbdaasbvcydsaabdgsaayre", . */
+let test = "abdsgyhidsaadgbdaasbvcydsaabdgsaayre";
+let checkStr = test.split("");
+let setofa = [];
+let setofb = [];
+let setofc = [];
+let setofy = [];
